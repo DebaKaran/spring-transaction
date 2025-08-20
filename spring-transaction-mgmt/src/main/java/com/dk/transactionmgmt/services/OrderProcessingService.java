1e8f62d727e90286d1954f75ce3fb316732115cb
DebaKaran <karan.deba@gmail.com>
2025-08-20 17:19:48 +0530

    feat(order): add order placement flow with swagger integration
    - Added data.sql with initial Product seed data:
      INSERT INTO Product (name, price, stockQuantity) VALUES
        ('Laptop', 25000.0, 10),
        ('Smartphone', 5000.0, 12);
    
    - Updated application.yaml:
      • Configured server port (9191)
      • Enabled defer-datasource-initialization
      • Added SQL init properties to load data.sql
      • Linked Docker Compose file (spring-transaction-mgmt/compose.yaml)
    
    - Implemented OrderProcessingService:
      • Handles order placement
      • Validates stock availability
      • Calculates total price
      • Updates inventory
    
    - Added OrderProcessingController:
      • POST /api/orders endpoint
      • Allows placing an order via Swagger UI
    
    - Added Lombok version (1.18.34) in pom.xml

--- spring-transaction-mgmt/src/main/java/com/dk/transactionmgmt/services/OrderProcessingService.java
+++ spring-transaction-mgmt/src/main/java/com/dk/transactionmgmt/services/OrderProcessingService.java
@@ -1,9 +1,12 @@
 package com.dk.transactionmgmt.services;
 
 import com.dk.transactionmgmt.entities.Order;
+import com.dk.transactionmgmt.entities.Product;
 import com.dk.transactionmgmt.handlers.InventoryHandler;
 import com.dk.transactionmgmt.handlers.OrderHandler;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OrderProcessingService {
 
     private final OrderHandler orderHandler;
@@ -16,6 +19,29 @@ public class OrderProcessingService {
     }
 
     public Order placeAnOrder(Order order) {
-        return null;
+        // 1: get product from inventory
+        Product product =  inventoryHandler.getProduct(order.getProductId());
+
+        // 2: validate stock availability <(5)
+        if(order.getQuantity() > product.getStockQuantity()) {
+            throw new RuntimeException("insufficent stock available");
+        }
+
+        //3: update total price in order entity
+        order.setTotalPrice(order.getQuantity() * product.getPrice());
+
+        //4: save order
+        Order savedOrder = orderHandler.saveOrder(order);
+
+        //5: update stock in inventory
+        updateInventoryStock(order, product);
+
+        return savedOrder;
+    }
+
+    private void updateInventoryStock(Order order, Product product) {
+        int availableStock = product.getStockQuantity() - order.getQuantity();
+        product.setStockQuantity(availableStock);
+        inventoryHandler.updateProductDetails(product);
     }
 }
---
+++ spring-transaction-mgmt/src/main/resources/database/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Product (name,price,stockQuantity) VALUES
+	 ('Laptop',25000.0,10),
+	 ('Samrtphone',5000.0,12);
